bb2f072d53ca1d130dea5f05a7c39602d8058ad0
Yash Bansod <yashbansodgit@gmail.com>
2026-02-24 23:34:06 +0530

    core: add initial migration for Employee model and update app references in settings and urls

--- backend/hrms/urls.py
+++ backend/hrms/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/api/employees/", include("employee.urls")),
+    path("/api/employees/", include("employees.urls")),
 ]
