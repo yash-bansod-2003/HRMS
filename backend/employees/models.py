f1cf4d2bbc9ea7586b0390b5124741cbcbf10afb
Yash Bansod <yashbansodgit@gmail.com>
2026-02-24 23:36:29 +0530

    core: add created_at and updated_at fields to Employee model and migration

--- backend/employees/models.py
+++ backend/employees/models.py
@@ -13,6 +13,8 @@ class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     department = models.CharField(max_length=50, choices=department_choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name}"
