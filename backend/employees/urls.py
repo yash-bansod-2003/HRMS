34ab196780e156a03cbe2da50c6033efd55843be
Yash Bansod <yashbansodgit@gmail.com>
2026-02-25 00:22:32 +0530

    refactor: remove unused imports in tests and URLs files

--- backend/employees/urls.py
+++ backend/employees/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
+
 from .views import EmployeeViewSet
 
 router = DefaultRouter()
-router.register(r'employees', EmployeeViewSet, basename='employees')
+router.register(r"employees", EmployeeViewSet, basename="employees")
 
 urlpatterns = router.urls
