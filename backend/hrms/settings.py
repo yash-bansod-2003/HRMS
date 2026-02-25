8eaa4533828e733c9308dd5e57eaf60973732989
Yash Bansod <yashbansodgit@gmail.com>
2026-02-25 14:10:28 +0530

    feat: integrate dj-database-url for database configuration and update DATABASES setting

--- backend/hrms/settings.py
+++ backend/hrms/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -81,10 +82,10 @@ WSGI_APPLICATION = "hrms.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
 
 
